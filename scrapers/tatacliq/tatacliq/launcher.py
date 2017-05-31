abd388512d7b5be1692605f493a9e140de97c28a
yuri <yury0051@gmail.com>
2017-05-31 03:14:55 +0300

    new commit

--- scrapers/tatacliq/tatacliq/launcher.py
+++ scrapers/tatacliq/tatacliq/launcher.py
@@ -5,6 +5,17 @@ import logging
 from scrapy.utils.log import configure_logging
 from spiders.tatacliq_spider1 import MySpider1
 from spiders.tatacliq_spider2 import MySpider2
+from spiders.tatacliq_spider3 import MySpider3
+from spiders.tatacliq_spider4 import MySpider4
+from spiders.tatacliq_spider5 import MySpider5
+from spiders.tatacliq_spider6 import MySpider6
+from spiders.tatacliq_spider7 import MySpider7
+from spiders.tatacliq_spider8 import MySpider8
+from spiders.tatacliq_spider9 import MySpider9
+from spiders.tatacliq_spider10 import MySpider10
+from spiders.tatacliq_spider11 import MySpider11
+from spiders.tatacliq_spider12 import MySpider12
+from spiders.tatacliq_spider13 import MySpider13
 
 configure_logging(install_root_handler=False)
 logging.basicConfig(
@@ -17,5 +28,16 @@ logging.basicConfig(
 process = CrawlerProcess(get_project_settings())
 process.crawl(MySpider1)
 process.crawl(MySpider2)
+process.crawl(MySpider3)
+process.crawl(MySpider4)
+process.crawl(MySpider5)
+process.crawl(MySpider6)
+process.crawl(MySpider7)
+process.crawl(MySpider8)
+process.crawl(MySpider9)
+process.crawl(MySpider10)
+process.crawl(MySpider11)
+process.crawl(MySpider12)
+process.crawl(MySpider13)
 process.start()
 
